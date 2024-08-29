1af58cb50cee7ea596bedd5d3c7df5ff3d19e6ca
Riju Mondal <contact.rijumondal@gmail.com>
2024-08-29 19:03:40 +0530

    first commit

---
+++ src/test/java/com/FraudBustersInc/TransWatch/TransWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FraudBustersInc.TransWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
