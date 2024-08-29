558ce39a0d1d7bbbbc3248aa03e9df22af48d79c
Riju Mondal <contact.rijumondal@gmail.com>
2024-08-29 20:19:44 +0530

    initial architecture

---
+++ src/main/java/com/FraudBustersInc/TransWatch/v1/TransWatchApplication.java
@@ -0,0 +1,27 @@
+package com.FraudBustersInc.TransWatch.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class TransWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransWatchApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/api/v1")
+	public static class HelloWorldController {
+
+		@GetMapping("/hello_world")
+		public String helloWorld() {
+			return "Hello World";
+		}
+	}
+}
